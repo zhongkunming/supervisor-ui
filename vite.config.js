d43c2af6e8f5c0d14bfc068f6ae141ac48411b65
zhongkunming <73384930+zhongkunming@users.noreply.github.com>
2025-06-14 20:19:10 +0800

    build(vite): 配置 Element Plus 组件按需加载
    - 引入 AutoImport 和 Components插件
    - 使用 ElementPlusResolver 实现组件自动导入
    - 优化项目性能，减少冗余代码

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +14,16 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
