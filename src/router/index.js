ceab2544e5f2253f6dbbad85ccadc6494a649dae
zhongkunming <73384930+zhongkunming@users.noreply.github.com>
2025-06-17 11:21:39 +0800

    build(dependencies): 更新 axios 和 dayjs 依赖
    - 更新 axios版本至 1.10.0
    - 更新 dayjs版本至 1.11.13
    - 调整 LoginView 路由的名称为 "Login"
    - 更新 LoginView 组件的导入路径

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/login/login.vue'
+import LoginView from '@/views/login/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
-      name: 'login',
+      name: 'Login',
       component: LoginView,
     },
   ],
